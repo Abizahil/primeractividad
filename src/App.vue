fc1bef0537776527d3547734fa9a18c58844b175
unknown <2102077@utrivieramaya.edu.mx>
2024-09-20 19:06:32 -0500

    primerentrega

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+<h1> Las aventuras de mi gato Lucky </h1>
+</template>
+
+<style scoped>
+</style>
